fb8623c9117002c888a06dba3ce7a6f88e718593
kluchoo <dominikzucho@gmail.com>
2025-01-08 15:54:19 +0100

    Funkcja wykonująca całkowanie

--- main.cpp
+++ main.cpp
@@ -13,6 +13,20 @@ double f(double x) {
     return 4.0 / (1.0 + x * x);
 }
 
+// Funkcja wykonująca całkowanie na zadanym przedziale
+void integrate(double start, double end, int n) {
+    double width = (end - start) / n;
+    double local_sum = 0.0;
+
+    for (int i = 0; i < n; i++) {
+        double x = start + i * width;
+        local_sum += f(x) * width;
+    }
+
+    std::lock_guard<std::mutex> lock(mtx);
+    global_sum += local_sum;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
