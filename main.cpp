acb778040fe9e2e78fb6d96666b1c91423b5ce0b
kluchoo <dominikzucho@gmail.com>
2025-01-08 15:53:43 +0100

    Funkcja podcałkowa

--- main.cpp
+++ main.cpp
@@ -5,6 +5,14 @@
 #include <mutex>
 #include <iomanip>
 
+std::mutex mtx;
+double global_sum = 0.0;
+
+// Funkcja podcałkowa f(x) = 4/(1+x^2)
+double f(double x) {
+    return 4.0 / (1.0 + x * x);
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
